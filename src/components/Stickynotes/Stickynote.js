44d9866f0303a08687b98bef929818ec04a04d20
ANAMAYA VYAS <68732493+anamaya-vyas-zlatan@users.noreply.github.com>
2022-02-15 01:21:56 +0530

    Create Stickynote.js

---
+++ src/components/Stickynotes/Stickynote.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useState, useReducer } from "react";
+// import './StickynoteStyles.scss';
+import { v4 as uuid } from "uuid";
+
+import "./noteStyles.css";
+
+const initialNotesState = {
+  lastNoteCreated: null,
+  totalNotes: 0,
+  notes: [],
+};
+
+const notesReducer = (prevState, action) => {
+  switch (action.type) {
+    case "ADD_NOTE": {
+      const newState = {
+        lastNoteCreated: new Date().toTimeString().slice(0, 8),
+        totalNotes: prevState.notes.length + 1,
+        notes: [...prevState.notes, action.payload],
+      };
+
+      console.log("After ADD_NOTE: ", newState);
+      return newState;
+    }
+
+    case "DELETE_NOTE": {
+      const newState = {
+        ...prevState,
+        totalNotes: prevState.notes.length - 1,
+        notes: prevState.notes.filter((note) => note.id !== action.payload.id),
+      };
+
+      console.log("After DELETE_NOTE: ", newState);
+      return newState;
+    }
+  }
+};
+
+function Stickynote() {
+  const [noteInput, setNoteInput] = useState("");
+  const [notesState, dispatch] = useReducer(notesReducer, initialNotesState);
+  const addNote = (event) => {
+    event.preventDefault();
+
+    if (!noteInput) {
+      return;
+    }
+
+    const newNote = {
+      id: uuid(),
+      text: noteInput,
+      // rotate: Math.floor(Math.random() * 20),
+    };
+
+    dispatch({ type: "ADD_NOTE", payload: newNote });
+    setNoteInput("");
+  };
+
+  const dropNote = (event) => {
+    event.target.style.left = `${event.pageX - 50}px`;
+    event.target.style.top = `${event.pageY - 50}px`;
+  };
+
+  const dragOver = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  return (
+    <div className="app" onDragOver={dragOver} style={{ color: "#fff" }}>
+      <center>
+        {" "}
+        <h1>
+          Sticky notes- Remind yourself- priorities v/s necessities v/s luxury!
+        </h1>
+      </center>
+      <center>
+        <form onSubmit={addNote} className="note-form">
+          <textarea
+            value={noteInput}
+            onChange={(event) => setNoteInput(event.target.value)}
+            placeholder="Create a new note..."
+            style={{
+              // marginRight: "50px",
+              marginLeft: "25px",
+              // backgroundColor: "lightgrey",
+              fontFamily: "Verdana,sans-serif",
+              border: "1px solid lightgray",
+              height: "100px",
+              minWidth: "60%",
+              maxWidth: "60%",
+              padding: "10px",
+              outline: "none",
+            }}
+          ></textarea>
+          <button
+            style={{
+              border: "1px solid lightgray",
+              height: "50px",
+              minWidth: "61.45%",
+              maxWidth: "61.45%",
+              padding: "10px",
+              marginLeft: "25px",
+              marginTop: "-10px",
+              marginBottom: "10px",
+              backgroundColor: "#fff27f",
+              cursor: "pointer",
+              padding: "10px",
+              border: "0",
+              outline: "none",
+            }}
+          >
+            Add
+          </button>
+        </form>
+      </center>
+      {notesState.notes.map((note) => (
+        <div
+          className="note"
+          style={{ transform: `rotate(${note.rotate}deg)` }}
+          draggable="true"
+          onDragEnd={dropNote}
+          key={note.id}
+        >
+          <div
+            onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}
+            className="close"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+
+          <pre className="text">{note.text}</pre>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Stickynote;
